771441b56438a8e31353be9b32e9078c53019cfa
unknown <francis000cheng@gmai.com>
2020-01-02 20:46:56 +0800

    sdf

--- 归并排序.cpp
+++ 归并排序.cpp
@@ -4,7 +4,21 @@ const int maxn = 100;
 //recursion implementation
 void merge(int A[], int L1, int R1, int L2, int R2)
 {
-    
+    int i = L1, j=L2;
+    int temp[maxn], index = 0;
+    while(i<=R1 && j<=R2)
+    {
+        if(A[i] <= A[j])
+        {
+            temp[index++] = A[i++];
+        }else
+        {
+            temp[index++] = A[j++]
+        }
+        
+    }
+    while(i<=R1) temp[index++] = A[i++];
+    while()
 }
 
 int main()
